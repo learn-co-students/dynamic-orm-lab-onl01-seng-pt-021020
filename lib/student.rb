95160d99b60b825a318af78516d1033695be3474
pbedrosian <peter_bedrosian@Yahoo.com>
2020-04-18 17:40:44 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
-  self.column_names.each {|col_name| attr_accessor col_name.to_sym}
-
+    self.column_names.each {|col_name| attr_accessor col_name.to_sym} #goes over the inserted column_names and itterates over them to convert them to a symbol
 end
+
