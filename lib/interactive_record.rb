e63df6cd1811215a76e6c7d87e4e138715e84ba8
Amber Williams <awilliams1234@live.com>
2020-04-20 17:26:17 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,9 +28,25 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def col_name_for_insert
+  def col_names_for_insert
 
-  end 
+  end
+
+  def values_for_insert
+
+  end
+
+  def save
+
+  end
+
+  def self.find_by_name(name)
+
+  end  
+
+  def self.find_by
+
+  end
 
 
 end
